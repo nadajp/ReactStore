b93791e47545491dcc74a721f437a227e73227a1
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-04-19 11:41:39 -0400

    add stripe and list products

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,38 @@
+import ProductCard from "src/components/ProductCard";
+import { stripe } from "src/utils/stripe"
 
-export default function Home() {
-  return <div>home</div>
+export default function Home({ products }) {
+  return (
+    <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
+      <div className="grid gap-8 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
+        {products.map(product => (
+          <ProductCard key={product.id} product={ product }/>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const inventory = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 8,
+  });
+
+  const products = inventory.data.map(product => {
+    const price = product.default_price
+    return {
+      currency: price.currency,
+      id: product.id,
+      name: product.name,
+      price: price.unit_amount,
+      image: product.images[0]
+    }
+  })
+
+  return {
+    props: {
+      products
+    }
+  }
 }
