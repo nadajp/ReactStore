1b6bf208119943a1ae167a580e08471e166c19ae
Nada J. Pivcevic <nada.jaksic@gmail.com>
2023-04-19 15:26:14 -0400

    add product detail cards

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
+import Rating from "./Rating";
+import { formatCurrencyString } from 'use-shopping-cart';
 
 export default function ProductCard({ product, index }) {
     return (
@@ -8,14 +10,27 @@ export default function ProductCard({ product, index }) {
             className="border-2 rounded-md group overflow-hidden"
         >
             <div className="relative w-full h-64">
-                <Image priority = {index === 0} src={product.image} alt={product.name} fill sizes="100"/>
+                <Image 
+                  priority = {index === 0} 
+                  src={product.image} 
+                  alt={product.name} 
+                  fill 
+                  sizes="100"
+                  style={{
+                    objectFit: 'contain'
+                  }}
+                  />
             </div>
             <div className="p-6 bg-white">
                 <p className="font-semibold text-lg">{product.name}</p>
+                <Rating />
                 <div className="mt-4 flex items-center justify-between space-x-2">
                     <div>
                         <p className="text-gray-500 ">Price</p>
-                        <p className="text-lg font-semibold">{product.price}</p>
+                        <p className="text-lg font-semibold">{formatCurrencyString({
+                            currency: product.currency,
+                            value: product.price
+                        })}</p>
                     </div>
                     <button className="border rounded-lg py-1 px-4">
                     Add to Cart
